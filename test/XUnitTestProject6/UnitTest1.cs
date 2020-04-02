990e631f7d34a44d3d9dc44aa5392f65eb48a628
David Fowler <davidfowl@gmail.com>
2020-04-01 22:11:58 -0700

    Add more logs

--- test/XUnitTestProject6/UnitTest1.cs
+++ test/XUnitTestProject6/UnitTest1.cs
@@ -3,15 +3,25 @@ using System.Diagnostics;
 using System.IO;
 using System.Threading.Tasks;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace XUnitTestProject6
 {
     public class UnitTest1
     {
+        private ITestOutputHelper _testOutputHelper;
+
+        public UnitTest1(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+        }
+
         [Fact]
         public async Task OutOfProcessHang()
         {
             var path = Path.Combine(Path.GetDirectoryName(typeof(UnitTest1).Assembly.Location), "ProcessWithHang", "ProcessWithHang.dll");
+
+            _testOutputHelper.WriteLine($"About to execute: {path}");
 
             await ProcessUtil.RunAsync("dotnet", path);
         }
