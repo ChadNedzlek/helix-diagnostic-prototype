f63cd2f26daeac62b369fc07e67e83f2b9aa6536
David Fowler <davidfowl@gmail.com>
2020-04-01 17:21:06 -0700

    Add out of process hanging test

---
+++ test/XUnitTestProject6/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace XUnitTestProject6
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public async Task OutOfProcessHang()
+        {
+            var path = Path.Combine(Path.GetDirectoryName(typeof(UnitTest1).Assembly.Location), "ProcessWithHang", "ProcessWithHang.dll");
+
+            await ProcessUtil.RunAsync("dotnet", path);
+        }
+    }
+}
